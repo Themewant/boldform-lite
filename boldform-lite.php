28105c6c737dc0b4d11ed84885fad28721e04507
Ibrahim Riaz <ibrahimriaz.github@gmail.com>
2026-09-16 09:47:21 +0600

    Updated

--- boldform-lite.php
+++ boldform-lite.php
@@ -1,13 +1,14 @@
 <?php
 /**
  * Plugin Name:       BoldForm Lite
+ * Plugin URI:        https://wpboldform.com/
  * Description:       Lightweight drag and drop form builder for WordPress.
  * Version:           1.1.8
  * Requires at least: 6.3
- * Tested up to:      7.0
+ * Tested up to:      7.1
  * Requires PHP:      7.4
- * Author:      Themewant
- * Author URI:  http://themewant.com/
+ * Author:            Themewant
+ * Author URI:        https://themewant.com/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       boldform-lite
